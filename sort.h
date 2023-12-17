031c43bafccbf07454fc8d4edde2ee02e8dc0627
Sabir222 <mrsabir4@gmail.com>
2023-12-17 23:10:14 +0100

    lol

--- sort.h
+++ sort.h
@@ -16,17 +16,17 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
+// void print_array(const int *array, size_t size);
+// void print_list(const listint_t *list);
 listint_t *swap_node(listint_t *node, listint_t **list);
-void shell_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
+// void shell_sort(int *array, size_t size);
+// void cocktail_sort_list(listint_t **list);
+// void counting_sort(int *array, size_t size);
+// void merge_sort(int *array, size_t size);
+// void heap_sort(int *array, size_t size);
+// void radix_sort(int *array, size_t size);
+// void bitonic_sort(int *array, size_t size);
+// void quick_sort_hoare(int *array, size_t size);
 
 
 
