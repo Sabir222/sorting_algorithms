77be6f70120d13e4da9244274f5d4ab92badc920
Sabir222 <mrsabir4@gmail.com>
2023-12-17 23:25:45 +0100

    lol

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,47 +1,45 @@
 #include "sort.h"
 /**
- * insertion_sort_list - Sorting a doubly linked list because even lists
- *                       deserve to be in order!
- * @list: The majestic doubly linked list to untangle and sort
+ * insertion_sort_list - fu
+ * @list: D
  */
-
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *golang;
+	listint_t *node;
 
-	if (list == NULL || (*list)->nxxt == NULL)
+	if (list == NULL || (*list)->next == NULL)
 		return;
-	golang = (*list)->nxxt;
-	while (golang)
+	node = (*list)->next;
+	while (node)
 	{
-		while ((golang->ppreviuous) && (golang->ppreviuous->n > golang->n))
+		while ((node->prev) && (node->prev->n > node->n))
 		{
-			golang = swap_node(golang, list);
+			node = swap_node(node, list);
 			print_list(*list);
 		}
-		golang = golang->nxxt;
+		node = node->next;
 	}
 }
 /**
- * swap_node - Because sometimes nodes want to dance and swap places
- * @golang: The daring golang that wants to switch partners
- * @list: The dance floor – your doubly linked list
- * Return: A pointer to a golang that just couldn't resist the swap
+ *swap_node - swap a node for his previous one
+ *@node: node
+ *@list: node list
+ *Return: return a pointer to a node which was enter it
  */
-listint_t *swap_node(listint_t *golang, listint_t **list)
+listint_t *swap_node(listint_t *node, listint_t **list)
 {
-	listint_t *back = golang->ppreviuous, *cur = golang;
+	listint_t *back = node->prev, *current = node;
 	/*NULL, 19, 48, 9, 71, 13, NULL*/
 
-	back->nxxt = cur->nxxt;
-	if (cur->nxxt)
-		cur->nxxt->ppreviuous = back;
-	cur->nxxt = back;
-	cur->ppreviuous = back->ppreviuous;
-	back->ppreviuous = cur;
-	if (cur->ppreviuous)
-		cur->ppreviuous->nxxt = cur;
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
 	else
-		*list = cur;
-	return (cur);
+		*list = current;
+	return (current);
 }
--- sort.h
+++ sort.h
@@ -3,22 +3,22 @@
 
 #include <stdlib.h>
 /**
- * struct listint_s - Behold, the majestic Doubly Linked List golang!
+ * struct listint_s - Behold, the majestic Doubly Linked List node!
  *
- * @n: The magical integer stored in this golang
- * @ppreviuous: A pointer to the past – the wise predecessor of this golang
- * @nxxt: A pointer to the future – the promising successor of this golang
+ * @n: The magical integer stored in this node
+ * @prev: A pointer to the past – the wise predecessor of this node
+ * @next: A pointer to the future – the promising successor of this node
  */
 typedef struct listint_s
 {
 	const int n;
-	struct listint_s *ppreviuous;
-	struct listint_s *nxxt;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *arr, size_t size);
 void print_list(const listint_t *list);
-listint_t *swap_node(listint_t *golang, listint_t **list);
+listint_t *swap_node(listint_t *node, listint_t **list);
 void shell_sort(int *arr, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *arr, size_t size);
